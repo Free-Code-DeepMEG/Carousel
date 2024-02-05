5e1c0b6d65439d1c9d4bc6070997f012a774fafb
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2024-02-05 20:24:50 +0530

    add navbar javascript

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/*  MENU SHOW */    
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu') 
+    })
+}
+
+/* MENU HIDDEN */
+
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
+/*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 
 /*=============== CAROUSEL ANIMATION ===============*/
